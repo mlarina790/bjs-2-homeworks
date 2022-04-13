779c8176bcd9baafaa1a70161c0015101562dd0b
Marina <mlarina790@gmail.com>
2022-04-13 21:44:14 +0300

    homework1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -21,9 +21,7 @@ function solveEquation(a, b, c) {
   return arr;
  }
   
-  
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
+  function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
