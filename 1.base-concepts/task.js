4a5660f2e8e476f401624bca20092ecf6172e1c4
Marina <mlarina790@gmail.com>
2022-04-13 21:14:11 +0300

    homework 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,27 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  if ( a == 0)
+  return false;
+  let arr = {};
+  let D = b ** 2 - 4 * a * c;
+  console.log ('D = ' + D);
+
+    if (D > 0) {
+    let x = [];
+    x.push((-b + Math.sqrt(D)) / (2 * a));
+    x.push((-b - Math.sqrt(D)) / (2 * a));
+    arr = x;
+     } else if (D == 0) {
+    arr = (-b / (2 * a));
+     } else if (D < 0) {
+    return false;
+    arr = D;
+     };
+
+  return arr;
+ }
+  
+  
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
