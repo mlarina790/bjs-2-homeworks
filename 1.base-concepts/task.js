1b0c297b28fac48c2da801f2d5adbdded8edbf5f
Marina <mlarina790@gmail.com>
2022-04-20 21:44:46 +0300

    modified task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
-  if ( a == 0)
-  return false;
-  let arr = {};
-  let D = b ** 2 - 4 * a * c;
-  console.log ('D = ' + D);
-
-    if (D > 0) {
-    let x = [];
-    x.push((-b + Math.sqrt(D)) / (2 * a));
-    x.push((-b - Math.sqrt(D)) / (2 * a));
-    arr = x;
-     } else if (D == 0) {
-    arr = (-b / (2 * a));
-     } else if (D < 0) {
+  if ( a == 0) {
     return false;
-    arr = D;
-     };
-
-  return arr;
+  }
+    let arr = [];
+    let d = b ** 2 - 4 * a * c;
+      if (d == 0) {
+        arr.push(-b / (2 * a));
+      } else if (d > 0) {
+        arr.push((-b + Math.sqrt(d)) / (2 * a));
+        arr.push((-b - Math.sqrt(d)) / (2 * a));
+      }
+   return arr;
  }
   
   function calculateTotalMortgage(percent, contribution, amount, date) {
