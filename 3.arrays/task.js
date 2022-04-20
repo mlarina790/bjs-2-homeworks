747e42b817423648e64cd2377a29fba2abd45af2
Marina <mlarina790@gmail.com>
2022-04-20 21:15:25 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
-function compareArrays(arr1, arr2) {
-  let result;
+'use strict'
+ function compareArrays(arr1, arr2) {
+  let result = arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]); 
 
-  // Ваш код
-
-  return result; // boolean
+   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+    resultArr = arr.filter(item => item > 0 && item % 3 === 0).map(item => item * 10);
 
   return resultArr; // array
 }
