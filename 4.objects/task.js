257e1fca462706797b7c952739d4ab7d72d7e9cd
Marina <mlarina790@gmail.com>
2022-04-28 18:31:50 +0300

    added solution

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
+"use strict";
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...rest) {
+  if (this.marks === undefined) {
+    this.marks = [...rest];
+  } else {
+    this.marks.push(...rest);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  return this.marks.reduce((sum, mark) => (sum + mark)) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
